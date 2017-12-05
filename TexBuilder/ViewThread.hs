d7b68f4a6ccbdc416c638bfaa18125a4568826be
Dominik Xaver Hörl <hoe.dom@gmx.de>
2017-12-05 13:30:50 +0100

    Actively search for mupdf binary to improve portability.

--- TexBuilder/ViewThread.hs
+++ TexBuilder/ViewThread.hs
@@ -5,13 +5,15 @@ where
 import TexBuilder.Utils.BinSem
 
 import Data.Functor
+import Data.Maybe
 import Control.Monad
+import Control.Applicative
 import Control.Concurrent
 import System.Posix.Types
 import System.Posix.Signals
 import System.Process
 import System.Process.Internals
-
+import System.Directory
 
 
 mupdfView :: FilePath -- ^ The path of the file to view
@@ -20,11 +22,18 @@ mupdfView :: FilePath -- ^ The path of the file to view
   --   pdf view should be updated.
   -> IO ()
 mupdfView pdffile sem = do
-  ph <- spawnProcess "/usr/bin/mupdf" [pdffile]
+  ph <- do
+    mupdfPath <- getMupdfPath
+    spawnProcess mupdfPath [pdffile]
   Just pid <- getPid ph
   tid <- forkIO $ signalThread pid sem
   exCode <- waitForProcess ph
   killThread tid
+  where
+    getMupdfPath :: IO FilePath
+    getMupdfPath = fromMaybe (error "mupdf not found")
+      . foldr (<|>) Nothing
+      <$> mapM findExecutable [ "mupdf", "mupdf-x11" ]
 
 signalThread :: ProcessID -> BinSem -> IO ()
 signalThread pid sem = do
